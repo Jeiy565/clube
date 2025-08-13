7ebc6f63e159e18c17d60bb3ad008f8bfce4f112
Jeipy565 <joaopaulogontijoterra@gmail.com>
2025-08-13 14:53:05 -0300

    Create dia 1.py

---
+++ dia 1.py
@@ -0,0 +1,20 @@
+num1 = 4
+num2 = 5
+num3 = 6
+num4 = 76
+num5 = 64
+num6 = 71
+num7 = 67
+num8 = 54
+num9 = 46
+resultado3 = num6 - num5 + num4 + num2
+print(resultado3)
+resultado1 = num1 - num2
+print(resultado1)
+resultado2 = num2 + num3
+print(resultado2)
+resultado5 = num4 - num3 * num6 / num4 + num3 
+print(resultado5)
+
+resultado4 = num2 -num3 + num5 * num8 -num7 - num6
+print(resultado4)
