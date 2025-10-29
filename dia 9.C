82b1e75242c4fb474d710ade10d2dce6b523937b
Jeipy565 <joaopaulogontijoterra@gmail.com>
2025-10-29 14:58:29 -0300

    Create dia 9.C
    Added a function to sum two numbers and print the result. Updated main to iterate and print numbers.

---
+++ dia 9.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void soma (){
+    int num1, num2, num3, resultado;
+    
+    printf("Digite dois numeros:");
+
+    scanf("%d %d",&num1, &num2);
+    
+    
+    resultado = num1 + num2;
+    
+    
+    printf("O resultado da soma de %d e %d e iqual a %d", num1, num2, resultado);   
+}
+int main(){
+    
+    for (int i = 0;  ; i++){
+        
+    if (i == 51){
+        return 0;
+        
+}
+        else {printf("%d\n", i);
+    
+            
+}
+        
+        
+}
+    
